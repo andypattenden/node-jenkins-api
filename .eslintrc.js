4d4e40cf30d0c68ff4654a589fc4d41097e3da91
Andy Pattenden <andy.pattenden@specialistholidays.com>
2017-09-08 11:48:27 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    
+    "extends": [
+        "eslint:recommended"
+    ],
+    
+    "plugins": [
+        "prettier"
+    ],
+
+    "rules": {
+        "prettier/prettier": "error"
+    }
+};
---
+++ README.md
@@ -0,0 +1,16 @@
+# jenkins-node-api
+
+A Node.js wrapper for the Jenkins API
+
+> **Note**: This is still in alpha
+
+## Installation
+
+Install with the node package manager [npm](http://npmjs.org):
+
+	$ npm install
+
+
+## Usage
+
+TODO: Describe usage
